46b70ec79311989bdef74c48b5a1ebf53ec9d96f
Andy J. Wills <andy@willslab.co.uk>
2018-07-13 15:49:48 +0100

    Draft 1 of IRR worksheet.

---
+++ src/make-irr.R
@@ -0,0 +1,24 @@
+## IRR data set on friendship
+## Categories from Gummerum & Keller (2008)
+library(tidyverse)
+content <- c("Shared Act", "Helping", "Trust", "Feelings")
+
+nsubj <- 5
+subj1 <- 56
+pag <- .6
+
+rater1 <- content[round(runif(nsubj, 1, length(content)), 0)]
+rater2 <- rater1
+
+for(i in 1:length(rater1)) {
+  if(runif(1,0,1) > pag) {
+    rater2[i] <- content[round(runif(1, 1, length(content)), 0)]
+  }
+}
+
+subj <- subj1:(subj1+nsubj-1)
+
+rats <- data.frame(subj, rater1, rater2)
+
+write_csv(rats,"irr.csv")
+
